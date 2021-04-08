bdd8704e5953fc891df8c5ea2f3bc908bea62771
Izabela1990 <76615147+Izabela1990@users.noreply.github.com>
2021-04-08 11:59:38 +0200

    first commit

---
+++ array.js
@@ -0,0 +1,14 @@
+let bubbles = [60, 59, 48, 71, 45, 51, 65, 70, 49, 38]
+
+let fruits = ["ananas","mango","orange","tomato","cucumber"]
+
+console.log(bubbles.sort())
+console.log(bubbles)
+bubbles.sort()
+ max = bubbles.pop()
+
+ fruits.sort()
+ fruits.reverse()
+
+ console.log(max)
+ console.log(fruits)
